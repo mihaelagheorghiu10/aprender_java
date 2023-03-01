5f3a553c26ec5273d1bede9faf2f65f479361415
mihaelagheorghiu10 <118915363+mihaelagheorghiu10@users.noreply.github.com>
2023-03-01 11:07:04 +0100

    exercise 2 Java

--- src/intro/Java/ByeWorld.java
+++ src/intro/Java/ByeWorld.java
@@ -6,5 +6,41 @@ public class ByeWorld {
         //añade un tipo de comentario
 
         System.out.println("adios mundo desde clase ByeWorld");
+
+        // formar variable
+        //tipo de datos  identificador = ex. 30:
+        //tipo identificador;
+        //identificador=30;
+
+        //tipo de datos: enteros
+        byte number1= 1;//1 byte
+        short number2 = 2;//2 byte
+        int number3= 3;//4 byte
+        long number4= 4L;//8 byte
+
+        //punto flotante
+        float decimal1= 4,9f;
+        double decimal2= 9,99d;
+
+        //caracter
+        char caracter1= 'a';
+
+        //boleanos
+        boolean verdadero= true;
+        boolean falso= false;
+
+        //cadenas de texto
+        String "nombre"= "Mika";
+        String "apellido"= "Gheorghiu";
+
+        //tipos envoltorio
+        Integer numero= null;
+        Long numero2= 2L;
+
+
+
+
+
+
     }
 }
