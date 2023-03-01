1329c848c67d1e29b91c90b650a7f82a2e43b50e
mihaelagheorghiu10 <118915363+mihaelagheorghiu10@users.noreply.github.com>
2023-03-01 10:37:38 +0100

    exercise 1 java

---
+++ src/intro/Java/ByeWorld.java
@@ -0,0 +1,10 @@
+package intro.Java;
+
+public class ByeWorld {
+
+    public static void main(String[] args) {
+        //añade un tipo de comentario
+
+        System.out.println("adios mundo desde clase ByeWorld");
+    }
+}
---
+++ src/intro/Java/HelloWorld.java
@@ -0,0 +1,33 @@
+package intro.Java;
+
+public class HelloWorld {
+
+    /**
+     * comentarios de mantenimiento
+     * funcion principal punto de entrada a la aplicacion
+     * @param args
+     * @
+     */
+    public static void main(String[] args) {
+
+        /* aqui tienes un comentario*/
+
+        /*
+          comentarios tipo lista:
+          1. abrir fichero.
+          2. leer fichero.
+          3. procesar fichero.
+          4. etc
+
+         */
+
+        // imprimir texto - otro tipo de comentario
+        System.out.println("hello mundo"); // imprimir texto
+
+        //hasta aqui con los comentarios
+
+
+        System.out.println("hello mundo desde clase HelloWorld");
+    }
+
+}
