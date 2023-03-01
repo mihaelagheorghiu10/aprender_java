e88adcc6fd9e07dd275fc43b69028a097de6e8aa
mihaelagheorghiu10 <118915363+mihaelagheorghiu10@users.noreply.github.com>
2023-03-01 12:26:43 +0100

    exercise 3 Java

---
+++ src/intro/Java/Operadores.java
@@ -0,0 +1,54 @@
+package intro.Java;
+
+public class Operadores {
+    public static void main(String[] args);
+
+    //operadores
+
+    //aritmeticos
+
+    // + - / * %
+    int number1 = 1;
+    int number2 = 2;
+
+    int resultado1= number1 + number2 ;
+    int resultado2 = number1 -number2;
+    int resultado3= number1 * number2 ;
+    int resultado4= number1 / number2 ;
+
+    //logicos,boleanos,relacion,comparacion;
+    /*
+    >
+    >=
+    <
+    <=
+    ==
+    !=
+    && and
+    ||o
+    ! (true)
+     */
+
+    //asignacion
+    /*
+    =
+    +=
+    -=
+    *=
+    /=
+    %=
+     */
+
+    //incremento
+    //++ (en los bucles for)
+
+    //decremento
+    //-- ( en los bucles for)
+
+    //concatenacion
+    //+ (strings)
+
+
+
+
+}
