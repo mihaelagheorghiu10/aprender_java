5539e9043932d319a206ab92159534cfe4999363
mihaelagheorghiu10 <118915363+mihaelagheorghiu10@users.noreply.github.com>
2023-03-01 15:39:50 +0100

    Funciones

--- src/intro/Java/Funciones.java
+++ src/intro/Java/Funciones.java
@@ -2,14 +2,28 @@ package intro.Java;
 
 public class Funciones {
     public static void main(String[] args) {
-        holaMundo();
-        holaMundo();
+        // holaMundo();
+        // holaMundo();
+
+        //holaMundo("Mika");
+        //holaMundo("Maria");
+
+        String hola = devolverHola();
+        System.out.println(hola);
 
     }
+
     private static void holaMundo() {
-        System.out.println("hola mundo");
-        System.out.println("hola mundo");
+        System.out.println("Hola mundo");
+        System.out.println("Hola mundo");
 
     }
 
+    private static void holaMundo(String name) {
+        System.out.println("Hola" + name);
+    }
+
+    private static String devolverHola() {
+        return "Hola";
+    }
 }
