899c693e82f72a1c146a74c3ebcd4e6c2bdf132d
mihaelagheorghiu10 <118915363+mihaelagheorghiu10@users.noreply.github.com>
2023-03-02 11:10:35 +0100

    IfElse 1

---
+++ src/intro/Java/IfElse.java
@@ -0,0 +1,19 @@
+package intro.Java;
+
+public class IfElse {
+    public static void main(String[] args) {
+
+        boolean check = 5 < 10;
+
+        int number1 = 5;
+        int number2 = 10;
+        int number3 = 20;
+
+        if (number1 < number2 &&number2 < number3){
+            System.out.println("verdadero");
+        }
+
+        //if (check) {
+           // System.out.println("verdadero");
+        }
+    }
