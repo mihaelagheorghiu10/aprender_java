4a5a4ebebcd03ad3d8603746cfc4a19ad96bd56f
mihaelagheorghiu10 <118915363+mihaelagheorghiu10@users.noreply.github.com>
2023-03-02 11:50:05 +0100

    Bucle for

---
+++ src/intro/Java/FoorLoop.java
@@ -0,0 +1,18 @@
+package intro.Java;
+
+public class FoorLoop {
+    public static void main(String[] args) {
+
+        //for (int i = 0; 1 < 10; i++) {
+            //System.out.println("Hola mundo");
+        //}
+
+
+        for (int i = 0; 1 <= 10; i++) {
+                System.out.println(i + "Hola mundo");
+        }
+        //System.out.println("fin");
+        //}
+    }
+
+
