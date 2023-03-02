42655bab6fb6157a3f1fa27d0c0145c3a09c1f15
mihaelagheorghiu10 <118915363+mihaelagheorghiu10@users.noreply.github.com>
2023-03-02 11:24:07 +0100

    IfElse 2

--- src/intro/Java/IfElse.java
+++ src/intro/Java/IfElse.java
@@ -5,13 +5,24 @@ public class IfElse {
 
         boolean check = 5 < 10;
 
-        int number1 = 5;
+        int number1 = 50;
         int number2 = 10;
         int number3 = 20;
+        int number4 = 1000;
 
-        if (number1 < number2 &&number2 < number3){
+
+        if (number1 < number2 && number2 < number3) {
             System.out.println("verdadero");
+            System.out.println("verdadero");
+        }else if (number3 < number4){
+        System.out.println("else if");
+        }else{
+
         }
+
+        System.out.println("fin");
+
+
 
         //if (check) {
            // System.out.println("verdadero");
